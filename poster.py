8bf16a1a9264004a0119d72567e3976253f55986
pocodekuyayky-coder <t3daime@gmail.com>
2026-05-23 10:38:56 +0900

    Twitter API v2に戻す

--- poster.py
+++ poster.py
@@ -4,13 +4,12 @@ import os
 def post_to_twitter(text):
     """X(Twitter)に投稿する"""
     
-    auth = tweepy.OAuth1UserHandler(
+    client = tweepy.Client(
         consumer_key=os.getenv("TWITTER_API_KEY"),
         consumer_secret=os.getenv("TWITTER_API_SECRET"),
         access_token=os.getenv("TWITTER_ACCESS_TOKEN"),
         access_token_secret=os.getenv("TWITTER_ACCESS_SECRET")
     )
     
-    api = tweepy.API(auth)
-    api.update_status(status=text)
+    client.create_tweet(text=text)
     print("✅ Xへの投稿が完了しました！")
